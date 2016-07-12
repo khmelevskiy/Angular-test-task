6543e3b797e8c9bc525efb1d84c99269f737146a
Aleksandr Khmelevskiy <alexander.khmelevskiy@gmail.com>
2016-07-12 22:42:27 +0300

    add pagination

--- app/objects/states/list.js
+++ app/objects/states/list.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular
-  .module('app.objects.states.list', ['app.objects.services'])
+  .module('app.objects.states.list', ['app.objects.services','angularUtils.directives.dirPagination'])
   .config(addRoute);
 
 function addRoute($stateProvider, $urlRouterProvider) {
@@ -17,4 +17,12 @@ function addRoute($stateProvider, $urlRouterProvider) {
 
 function ObjectsListController($scope, ObjectsService) {
   $scope.collection = ObjectsService.getAll();
+
+  $scope.currentPage = 1;
+  $scope.pageSize = 7;
+  $scope.meals = [];
+
+  $scope.pageChangeHandler = function(num) {
+    console.log('going to page ' + num);
+  };
 }
